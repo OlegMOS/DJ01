605636712be58d3e5dcb005c0c8c5675cf9c147a
OlegM <x2007@rambler.ru>
2025-01-09 00:22:18 +0500

    Dj02 из Dj01

--- MyDjango/main/urls.py
+++ MyDjango/main/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.index), path('data', views.data), path('test', views.test)
-]
+    path('', views.index, name='home'), path('data', views.data, name='page3'), path('test', views.test, name='page4'),
+    path('blok', views.blok, name='page2')
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- MyDjango/main/views.py
+++ MyDjango/main/views.py
@@ -5,8 +5,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>Это главная страница</h1>")
+    return render(request, 'main/index.html')
+def blok(request):
+    return render(request, 'main/blok.html')
 def data(request):
-    return HttpResponse("<h1>Это страница Data моего проекта на Django</h1>")
+    return render(request, 'main/data.html')
 def test(request):
-    return HttpResponse("<h1>Это страница Test моего проекта на Django</h1>")
+    return render(request, 'main/test.html')
